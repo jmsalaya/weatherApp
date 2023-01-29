7fedc312eae993887236507f7f7111d30f99a52e
John Mark Salaya <jmsalaya3@gmail.com>
2023-01-29 22:20:59 +0800

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "abd20661bcmsh9e0f73e459100fbp1b2e13jsnbcc0ab352508",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d87d4a591a284f5409bcc4cb3646c875";
